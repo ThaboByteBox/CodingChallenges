2b80e1a5a1567de7d26d1766d3d9d210fed9b6c7
ThaboByteBox <144127310+ThaboByteBox@users.noreply.github.com>
2023-09-05 16:40:14 +0200

    Skeleton Code For Random Text Challenge
    feat: mainly skeleton code of challenge created
    feat: still need to add functionality to every function, styling for display element and buttons

---
+++ Challenge(Reworked).js
@@ -0,0 +1,34 @@
+let iLineCounter = 0
+let arrTextLine = []
+let isValid = true
+function addLineHandler () {
+    iLineCounter ++
+    function generateRandomLine(){
+        function generateRandomColour () {
+
+        }
+        function generateRandomText () {
+
+        }
+    }
+let textLine = generateRandomLine()
+if (iLineCounter > 9) {
+ 
+} else if (iLineCounter < 9) {
+
+}
+
+} 
+
+function removeLineHandler() {
+    if(isValid = true) {
+        function removeChild () {
+
+        }
+
+    }else if (isValid = false) {
+        function removeChild() {
+            
+        }
+    }
+}
