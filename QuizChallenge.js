68c640579665f71a6348c4a6a9f4131d73acd524
ThaboByteBox <144127310+ThaboByteBox@users.noreply.github.com>
2023-09-06 14:50:18 +0200

    Update on Quiz Challenge
    Update on Quiz Challenge 
    
    feat outstanding: still need to allow for cross referencing to check if the guess made by the user is the correct one
    docs: still need documentation using JSDocs

--- QuizChallenge.js
+++ QuizChallenge.js
@@ -3,23 +3,64 @@ let arrOptions = ["Bucharest","Helsinki","Paris"]
 let arrCorrectOption = [0,1,2]
 let displayEl = document.getElementById("questionText")
 
+document.getElementById("firstAnswer").style.visibility = "hidden"
+document.getElementById("secondAnswer").style.visibility = "hidden"
+document.getElementById("thirdAnswer").style.visibility = "hidden"
 
-displayEl.textContent = arrQuestionText[0] + "?"
-
-document.getElementById("firstAnswer").innerText = arrOptions[0]
-document.getElementById("secondAnswer").innerText = arrOptions[1]
-document.getElementById("thirdAnswer").innerText = arrOptions[2]
-
-function makeGuess() {
-    document.getElementById("firstAnswer").value = arrOptions[0] 
-    document.getElementById("secondAnswer").value = arrOptions[1]
-    document.getElementById("thirdAnswer").value = arrOptions[2]
-
-    if (document.getElementById("firstAnswer").value == document.getElementById("firstAnswer").innerText) {
-       document.getElementById("questionHealth").textContent = "Correct" 
-    } else {
-        document.getElementById("questionHealth").textContent = "Incorrect!, the correc answer is: " + arrOptions[0]  
+function beginQuiz() {
+    
+    for (i = 0; i < arrQuestionText.length; i++ ) {
+        document.getElementById("firstAnswer").style.visibility = "visible"
+        document.getElementById("secondAnswer").style.visibility = "visible"
+        document.getElementById("thirdAnswer").style.visibility = "visible"
+    
+        document.getElementById("firstAnswer").innerText = arrOptions[0]
+        document.getElementById("secondAnswer").innerText = arrOptions[1]
+        document.getElementById("thirdAnswer").innerText = arrOptions[2]
+    
+        document.getElementById("startQuiz").style.visibility = "hidden"
+    return  displayEl.textContent = arrQuestionText[i] + "?"  
+}   
     }
+   
+    
+
+
+
+
+
+function firstAnswerGuess() {
+    let answer = document.getElementById("firstAnswer").innerText
+    let guessHealthEl = document.getElementById("questionHealth").textContent
+    if (answer == arrOptions[0]) {
+    guessHealthEl = "Correct"
+    } else {
+    guessHealthEl = "Incorrect! The correct Answer is:  " + answer
+    }
+    console.log(answer)
+}
+
+function secondAnswerGuess() {
+    i = 0
+    let answer = document.getElementById("secondAnswer").innerText
+    let guessHealthEl = document.getElementById("questionHealth").textContent
+    if (answer == arrOptions[i]) {
+      guessHealthEl = "Correct"
+    } else {
+     guessHealthEl = "Incorrect! The correct Answer is:  " + answer
+    }
+}
+
+function thirdAnswerGuess() {
+    let answer = document.getElementById("thirdAnswer").innerText
+    console.log(answer)
+    let guessHealthEl = document.getElementById("questionHealth").textContent
+    if (answer == arrOptions[2]) {
+    guessHealthEl = "Correct"
+    } else {
+    guessHealthEl = "Incorrect! The correct Answer is:  " + answer
+    }
+    //console.log(answer)
 }
 
 
