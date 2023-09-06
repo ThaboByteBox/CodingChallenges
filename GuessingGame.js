fca690c6e3c1a69dcb8fd9c010f7a92a60e49ed8
ThaboByteBox <144127310+ThaboByteBox@users.noreply.github.com>
2023-09-06 10:19:36 +0200

    Update on Number Guessing Game
    Progress Report
    
    docs: added some documentation for functions
    feat: still need to add guesses to a html item

--- GuessingGame.js
+++ GuessingGame.js
@@ -1,17 +1,27 @@
 
 let secretNumber = Math.floor(Math.random() * 100) + 1
+//secretNumber = 15
 let arrGueses = []
 let attempts = 0
 
+
+
+
+/**
+ * uses IF loops to determine if what the user guesses is the correct random number generated
+ * @returns {string} alert depending on user's guess, either too high, low or correct 
+ */
 function makeGuess() {
     attempts++
     let userGuess = document.querySelector("input").value
-    //console.log(userGuess)
+    
+   
     if (userGuess < secretNumber) {
         document.body.style.backgroundColor = "red"
         return alert("Too low!, Try Again")
     } else if (userGuess == secretNumber) {
         document.body.style.backgroundColor = "green"
+        document.getElementById("checkGuess").style.visibility = "hidden"
         return alert("Congratulations! You guessed the correct number in: " + attempts + " attempts")
     } else if (userGuess > secretNumber){
         document.body.style.backgroundColor = "red"
@@ -19,6 +29,12 @@ function makeGuess() {
     }
    arrGueses.push(userGuess)
 
+    const guessPara = document.createElement("p")
+    const guessNode = document.createTextNode(arrGueses)
+    guessPara.appendChild(guessNode)
+
+    const guessListEL = document.getElementById("guessList")
+    guessListEL.appendChild(guessPara)
 }
 
 function resetGame () {
@@ -27,4 +43,5 @@ function resetGame () {
     arrGueses = []
     document.querySelector("input").value = ""
     document.body.style.backgroundColor = "white"
+    document.getElementById("checkGuess").style.visibility = "visible"
 }
