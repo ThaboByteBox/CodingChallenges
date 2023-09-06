db6d5512fb936002c713d36c2770ff27dc1b6010
ThaboByteBox <144127310+ThaboByteBox@users.noreply.github.com>
2023-09-06 13:12:47 +0200

    Update on Challenge
    Update on Challenge
    
    docs: added documentation to all functions

--- Challenge(Reworked).js
+++ Challenge(Reworked).js
@@ -1,12 +1,29 @@
 let iLineCounter = 0
 let arrTextLine = []
 let isValid = true
+
+/**
+ * @returns fully randomised line of text with randomised colour and adds it to the HTML display element and
+ * keeps track if the max number of entries have been added, max number being 9, disables the button if max number is
+ * reach
+ */
 function addLineHandler () {
     iLineCounter ++
+
+/**
+ * @returns complete radomised line of text with a radomised colour assinged to it 
+ */
     function generateRandomLine(){
+
+/**
+ * @returns randomly generated colour using hex codes
+ */
         function generateRandomColour () {
 
         }
+/**
+ * @returns randomly generated text 
+ */
         function generateRandomText () {
 
         }
@@ -20,6 +37,9 @@ if (iLineCounter > 9) {
 
 } 
 
+/**
+ *  Removes the lastChild added to the HTML display element
+ */
 function removeLineHandler() {
     if(isValid = true) {
         function removeChild () {
