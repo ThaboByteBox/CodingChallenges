b4fbb14895198cf3140790b76c9b954c9885a909
ThaboByteBox <144127310+ThaboByteBox@users.noreply.github.com>
2023-09-08 13:53:56 +0200

    Update of Quizz Challenge
    Update of Quizz Challenge 
    feat: allowed questions to be picked at random, can detect if answer is correct, displays the correct prompt if answer is wrong
    docs: documentation using JDcos still needed for some functions

--- QuizChallenge.js
+++ QuizChallenge.js
@@ -1,33 +1,49 @@
-let arrQuestionText = ["What is the Capital City of Romania","What is the Capital City of Finland"]
-let arrOptions = ["Bucharest","Helsinki","Paris"]
-let arrCorrectOption = [0,1,2]
+//const arrQuestionText = { 1:"What is the Capital City of Romania",2:"What is the Capital City of Finland",3: "What is the Capital Cityof France"}
+const arrQuestionText = [
+    {"question": "What is the Capital City of Romania", "answer": "Bucharest" },
+    {"question": "What is the Capital City of Finland", "answer": "Helsinki" },
+    {"question": "What is the Capital City of France", "answer": "Paris"}
+]
 let displayEl = document.getElementById("questionText")
-
+let qHealthEl = document.getElementById("questionHealth")
 document.getElementById("firstAnswer").style.visibility = "hidden"
 document.getElementById("secondAnswer").style.visibility = "hidden"
 document.getElementById("thirdAnswer").style.visibility = "hidden"
+let iRandomNumber = 0
+iRandomNumber = Math.floor( Math.random()*2)
+let firtsQuestion = arrQuestionText[iRandomNumber]["question"]
+let correctAnswer = ""
+
+console.log(iRandomNumber)
+
 
 /**
  * enables the visibilty of the other buttons and hides the Begin game
  * @returns  Question from the arrQuestionTex array
  */
 function beginQuiz() {
-    
-    for (i = 0; i < arrQuestionText.length; i++ ) {
         document.getElementById("firstAnswer").style.visibility = "visible"
         document.getElementById("secondAnswer").style.visibility = "visible"
         document.getElementById("thirdAnswer").style.visibility = "visible"
     
-        document.getElementById("firstAnswer").innerText = arrOptions[0]
-        document.getElementById("secondAnswer").innerText = arrOptions[1]
-        document.getElementById("thirdAnswer").innerText = arrOptions[2]
+        document.getElementById("firstAnswer").innerText = arrQuestionText[0]["answer"]
+        document.getElementById("secondAnswer").innerText = arrQuestionText[1]["answer"]
+        document.getElementById("thirdAnswer").innerText = arrQuestionText[2]["answer"]
     
         document.getElementById("startQuiz").style.visibility = "hidden"
-    return  displayEl.textContent = arrQuestionText[i] + "?"  
+        
+       // console.log(Object.value(arrQuestionText))   
+       displayEl.textContent = firtsQuestion + "?"  
+       if (firtsQuestion == arrQuestionText[iRandomNumber]["question"]) {
+        correctAnswer = arrQuestionText[iRandomNumber]["answer"]
+    } else {
+        correctAnswer = ""
+    }    
 }   
-    }
    
     
+
+
 
 
 
@@ -35,47 +51,26 @@ function beginQuiz() {
 /**
  * Loads the value of the guess and check if the guess is correct 
  */
-function firstAnswerGuess() {
-    let answer = document.getElementById("firstAnswer").innerText
-    let guessHealthEl = document.getElementById("questionHealth").textContent
-    if (answer == arrOptions[0]) {
-    guessHealthEl = "Correct"
-    } else {
-    guessHealthEl = "Incorrect! The correct Answer is:  " + answer
-    }
-    //console.log(answer)
+function firstAnswerGuess() {  
+   if(correctAnswer == arrQuestionText[0]["answer"] ) {
+    qHealthEl.textContent = "Correct!"
+   } else {
+    qHealthEl.textContent = "Incorrect! The correct answer is: " + correctAnswer
+   }
 }
 
-/**
- * loads the value of the guess and checks if the guess is correct 
- */
-function secondAnswerGuess() {
-    i = 0
-    let answer = document.getElementById("secondAnswer").innerText
-    let guessHealthEl = document.getElementById("questionHealth").textContent
-    if (answer == arrOptions[i]) {
-      guessHealthEl = "Correct"
-    } else {
-     guessHealthEl = "Incorrect! The correct Answer is:  " + answer
-    }
-}
-/**
- * loads the value of the guess and checks if the guess is correct 
- */
-function thirdAnswerGuess() {
-    let answer = document.getElementById("thirdAnswer").innerText
-    console.log(answer)
-    let guessHealthEl = document.getElementById("questionHealth").textContent
-    if (answer == arrOptions[2]) {
-    guessHealthEl = "Correct"
-    } else {
-    guessHealthEl = "Incorrect! The correct Answer is:  " + answer
-    }
-    //console.log(answer)
+function secondAnswerGuess () {     
+   if(correctAnswer == arrQuestionText[1]["answer"]) {
+    qHealthEl.textContent = "Correct!"
+   } else {
+    qHealthEl.textContent = "Incorrect! The correct answer is: " + correctAnswer
+   }
 }
 
-
-
-
-
-
+function thirdAnswerGuess () {
+    if(correctAnswer == arrQuestionText[2]["answer"] ) {
+        qHealthEl.textContent = "Correct!"
+       } else {
+        qHealthEl.textContent = "Incorrect! The correct answer is: " + correctAnswer
+       } 
+}
